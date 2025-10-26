40c7ec6a03fdcb6fc840ca6bb84f523b33890d00
Jonasta1999 <jonasahooei@gmail.com>
2025-10-26 14:58:01 +0100

    env

--- imdb-web/next.config.ts
+++ imdb-web/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
       // Example: calling /api/movies?q=... on Next -> forwards to FastAPI /movies?q=...
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
